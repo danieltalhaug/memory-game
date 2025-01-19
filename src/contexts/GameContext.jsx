477c9856621dfc414cfe90442be9b5776f48a762
danieltalhaug <daniel.talhaug@outlook.com>
2025-01-19 17:36:24 +0100

    Create context for game

---
+++ src/contexts/GameContext.jsx
@@ -0,0 +1,33 @@
+import { useState, createContext } from 'react';
+
+export const GameContext = createContext();
+
+export function GameContextProvider({ children }) {
+    const defaults = {
+        playerName: 'Spiller',
+        gameType: {
+            type: 'colours',
+            text: 'Farger',
+        },
+        cardAmount: 4,
+    };
+    const gameTypeOptions = [
+        {
+            type: 'colours',
+            text: 'Farger',
+        },
+    ];
+    const cardAmountOptions = [4, 8, 12];
+
+    const [playerName, setPlayerName] = useState(defaults.playerName);
+    const [gameType, setGameType] = useState(defaults.gameType);
+    const [cardAmount, setCardAmount] = useState(defaults.cardAmount);
+
+    return (
+        <GameContext.Provider
+            value={[playerName, setPlayerName, gameType, setGameType, gameTypeOptions, cardAmount, setCardAmount, cardAmountOptions]}
+        >
+            {children}
+        </GameContext.Provider>
+    );
+};
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import ScrollToTop from '../components/ScrollToTop';
+import { GameContextProvider } from '../contexts/GameContext';
 
 const DefaultLayout = () => {
     return (
-        <>
+        <GameContextProvider>
             <ScrollToTop />
             <Outlet />
-        </>
+        </GameContextProvider>
     );
 };
 
