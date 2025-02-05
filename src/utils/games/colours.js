73539e9f082fb3bc7c053aa5552b702fe2c69764
danieltalhaug <daniel.talhaug@outlook.com>
2025-02-05 21:56:15 +0100

    Create data for Colour game

---
+++ src/utils/games/colours.js
@@ -0,0 +1,39 @@
+// CSS Colours https://www.w3schools.com/cssref/css_colors.php
+export default [
+    {
+        color: 'RebeccaPurple',
+    },
+    {
+        color: 'SpringGreen',
+    },
+    {
+        color: 'RoyalBlue',
+    },
+    {
+        color: 'CornflowerBlue',
+    },
+    {
+        color: 'Orange',
+    },
+    {
+        color: 'Red',
+    },
+    {
+        color: 'HotPink'
+    },
+    {
+        color: 'Brown',
+    },
+    {
+        color: 'DarkGoldenRod'
+    },
+    {
+        color: 'DarkOliveGreen',
+    },
+    {
+        color: 'Magenta',
+    },
+    {
+        color: 'OrangeRed',
+    },
+];
