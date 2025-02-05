eece599819b29ae929c9d1eabb2cc6029e869143
danieltalhaug <daniel.talhaug@outlook.com>
2025-02-05 21:53:36 +0100

    Add Memory title

--- src/pages/GameSetupPage.jsx
+++ src/pages/GameSetupPage.jsx
@@ -4,21 +4,24 @@ import FormGameSetup from '../components/FormGameSetup';
 const GameSetupPage = () => {
     return (
         <article className="bg-base-200 h-screen flex justify-center items-center">
-            <section className="card bg-base-100 w-96 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Lag spill
-                    </h2>
-                    <FormGameSetup />
-                    <span className="divider" />
-                    <footer className="card-actions justify-end">
-                        <Button
-                            color="accent"
-                            text="Start spill"
-                            to="/play"
-                        />
-                    </footer>
-                </div>
+            <section>
+                <h1 className="text-primary text-6xl text-center font-bold mb-8">MEMORY</h1>
+                <section className="card bg-base-100 w-96 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Lag spill
+                        </h2>
+                        <FormGameSetup />
+                        <span className="divider" />
+                        <footer className="card-actions justify-end">
+                            <Button
+                                color="accent"
+                                text="Start spill"
+                                to="/play"
+                            />
+                        </footer>
+                    </div>
+                </section>
             </section>
         </article>
     );
