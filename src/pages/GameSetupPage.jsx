f5ec4f257f080a39755cefda84771876e4d49bb1
danieltalhaug <daniel.talhaug@outlook.com>
2025-02-05 21:56:52 +0100

    Create Memory title to front page

--- src/pages/GameSetupPage.jsx
+++ src/pages/GameSetupPage.jsx
@@ -5,7 +5,9 @@ const GameSetupPage = () => {
     return (
         <article className="bg-base-200 h-screen flex justify-center items-center">
             <section>
-                <h1 className="text-primary text-6xl text-center font-bold mb-8">MEMORY</h1>
+                <h1 className="text-primary text-6xl text-center font-bold mb-8">
+                    MEMORY
+                </h1>
                 <section className="card bg-base-100 w-96 shadow-xl">
                     <div className="card-body">
                         <h2 className="card-title">
